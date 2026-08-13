7196cb90131d6445985c6c31a36a777b256fbe49
Jocelyn P. Mahlatse <kmahlatse123@gmail.com>
2026-08-13 22:59:45 +0200

    Create media.js

---
+++ src/config/media.js
@@ -0,0 +1,3 @@
+export const BACKGROUND_VIDEO = ""
+// When you have your Cloudinary video link, paste it here
+// Example: export const BACKGROUND_VIDEO = "https://res.cloudinary.com/your-cloud/video/upload/your-video.mp4"
